7b3d7164e4c26662906312d1471767c0edb51257
Aanya Alwani <aalwani@fb.com>
2016-06-17 13:41:41 -0700

    Initial commit

--- flix/DetailViewController.swift
+++ flix/DetailViewController.swift
@@ -28,9 +28,83 @@ class DetailViewController: UIViewController {
         overviewLabel.sizeToFit()
         if let posterPath = movie["poster_path"] as? String
         {
-            let imageURL = NSURL(string: baseURL + posterPath)
+            let imageUrl = baseURL + posterPath
+            let smallImageUrl = "https://image.tmdb.org/t/p/w45" + posterPath
+            let largeImageUrl = "https://image.tmdb.org/t/p/original" + posterPath
+            let smallImageRequest = NSURLRequest(URL: NSURL(string: smallImageUrl)!)
+            let largeImageRequest = NSURLRequest(URL: NSURL(string: largeImageUrl)!)
+            let imageRequest = NSURLRequest(URL: NSURL(string: imageUrl)!)
             
-            posterImageView.setImageWithURL(imageURL!)
+            
+            /*
+            self.posterImageView.setImageWithURLRequest(
+                imageRequest,
+                placeholderImage: nil,
+                success: { (imageRequest, imageResponse, image) -> Void in
+                    
+                    // imageResponse will be nil if the image is cached
+                    if imageResponse != nil {
+                        print("Image was NOT cached, fade in image")
+                        self.posterImageView.alpha = 0.0
+                        self.posterImageView.image = image
+                        UIView.animateWithDuration(0.3, animations: { () -> Void in
+                            self.posterImageView.alpha = 1.0
+                        })
+                    } else {
+                        print("Image was cached so just update the image")
+                        self.posterImageView.image = image
+                    }
+                },
+                failure: { (imageRequest, imageResponse, error) -> Void in
+                    // do something for the failure condition
+            })
+            
+            */
+            
+            
+            self.posterImageView.setImageWithURLRequest(
+                smallImageRequest,
+                placeholderImage: nil,
+                success: { (smallImageRequest, smallImageResponse, smallImage) -> Void in
+                    
+                    // smallImageResponse will be nil if the smallImage is already available
+                    // in cache (might want to do something smarter in that case).
+                    self.posterImageView.alpha = 0.0
+                    self.posterImageView.image = smallImage;
+                    
+                    UIView.animateWithDuration(0.3, animations: { () -> Void in
+                        
+                        self.posterImageView.alpha = 1.0
+                        
+                        }, completion: { (sucess) -> Void in
+                            
+                            // The AFNetworking ImageView Category only allows one request to be sent at a time
+                            // per ImageView. This code must be in the completion block.
+                            self.posterImageView.setImageWithURLRequest(
+                                largeImageRequest,
+                                placeholderImage: smallImage,
+                                success: { (largeImageRequest, largeImageResponse, largeImage) -> Void in
+                                    
+                                    self.posterImageView.image = largeImage;
+                                    
+                                },
+                                failure: { (request, response, error) -> Void in
+                                    // do something for the failure condition of the large image request
+                                    // possibly setting the ImageView's image to a default image
+                            })
+                    })
+                },
+                failure: { (request, response, error) -> Void in
+                    // do something for the failure condition
+                    // possibly try to get the large image
+            })
+ 
+            
+            
+
+           // let imageURL = NSURL(string: baseURL + posterPath)
+            
+            // posterImageView.setImageWithURL(imageURL!)
         }
        
 
--- flix/MoviesViewController.swift
+++ flix/MoviesViewController.swift
@@ -14,11 +14,11 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     var endpoint: String!
     @IBOutlet weak var errorLabel: UILabel!
     @IBOutlet weak var errorView: UIView!
+    @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
     var movies: [NSDictionary]?
-    var filteredData: [String]!
+    var filteredData: [NSDictionary]!
     var data = [String]()
-    @IBOutlet weak var searchBar: UISearchBar!
     override func viewDidLoad()
     {
         super.viewDidLoad()
@@ -28,8 +28,11 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         tableView.dataSource = self
         tableView.delegate = self
         errorView.hidden = true
+        searchBar.delegate = self
         loadDataFromNetwork()
+        
         if let navigationBar = navigationController?.navigationBar {
+            navigationBar.setBackgroundImage(UIImage(named: "hh"), forBarMetrics: .Default)
             navigationBar.tintColor = UIColor(red: 0, green: 0.5, blue: 4, alpha: 1)
            
             let shadow = NSShadow()
@@ -42,7 +45,6 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
                 NSShadowAttributeName : shadow
            ]
         }
-        
     }
 
     override func didReceiveMemoryWarning() {
@@ -52,27 +54,33 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
-       
+       /*
         if let movies = movies{
             return movies.count}
+        else {return 0}*/
+ 
+        if let filteredData = filteredData{
+            return filteredData.count}
         else {return 0}
-        //return filteredData.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
     {
-        let cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath) as! MoviesCell
         
-        let movie = movies![indexPath.row]
+        let cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath) as! MoviesCell
+        //var selectionStyle: UITableViewCellSelectionStyle
+        //cell.selectionStyle = .None
+        let backgroundView = UIView()
+        backgroundView.backgroundColor = UIColor.yellowColor()
+        cell.selectedBackgroundView = backgroundView
+        let movie = filteredData[indexPath.row]
         let title = movie["title"] as! String
         let overview = movie["overview"] as! String
         let baseURL = "http://image.tmdb.org/t/p/w500"
         cell.titleLabel.text = title
         cell.overviewLabel.text = overview
         if let posterPath = movie["poster_path"] as? String
-        {
-            let imageURL = NSURL(string: baseURL + posterPath)
-        
+        {           let imageURL = NSURL(string: baseURL + posterPath)
             cell.posterView.setImageWithURL(imageURL!)
         }
         return cell
@@ -101,6 +109,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
                 {
                     print("response:\(responseDictionary)")
                     self.movies = responseDictionary["results"] as! [NSDictionary]
+                    self.filteredData = self.movies
                     self.tableView.reloadData()
                 }
                 
@@ -120,21 +129,11 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         })
         task.resume()
         
+
         
     }
     
-    func getTitle()
-    
-    {
-        for index in 1...(movies!.count-1)
-        {
-        let moviesss = movies![index]
-        let title = moviesss["title"] as! String
-        data.append(title)
-        }
-        
-    }
-    func refreshControlAction(refreshControl: UIRefreshControl)
+        func refreshControlAction(refreshControl: UIRefreshControl)
     {
         self.loadDataFromNetwork()
         self.tableView.reloadData()
@@ -142,14 +141,52 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         refreshControl.endRefreshing()
     }
     
-    
-    
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String)
+    {
+        
+        // When there is no text, filteredData is the same as the original data
+        if searchText.isEmpty
+        {
+            filteredData = movies
+        }
+        else
+        {
+            
+            // The user has entered text into the search box
+            // Use the filter method to iterate over all items in the data array
+            // For each item, return true if the item should be included and false if the
+            // item should NOT be included
+            filteredData = movies!.filter({(x: NSDictionary) -> Bool in
+                // If dataItem matches the searchText, return true to include it
+                let title = x["title"] as! String
+                if title.rangeOfString(searchText, options: .CaseInsensitiveSearch) != nil
+                    {
+                        return true
+                    }
+                else
+                {
+                    return false
+                }
+            })
+            //let storage = movies
+            //movies = filteredData
+        }
+        tableView.reloadData()
+    }
+    func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
+        self.searchBar.showsCancelButton = true
+    }
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        searchBar.showsCancelButton = false
+        searchBar.text = ""
+        searchBar.resignFirstResponder()
+    }
     // This method updates filteredData based on the text in the Search Box
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?)
     {
         let cell = sender as! UITableViewCell
         let indexPath = tableView.indexPathForCell(cell)
-        let movie = movies![indexPath!.row]
+        let movie = filteredData![indexPath!.row]
         let detailViewController = segue.destinationViewController as! DetailViewController
         detailViewController.movie = movie
         
